9d150796297bc6d7769ebd677c3a94b2a11805d1
William <williamhdas@gmail.com>
2020-04-09 18:25:38 -0400

    Offsets

--- js/main.js
+++ js/main.js
@@ -147,7 +147,7 @@ $('.popup-video').magnificPopup({
     scrollTime: 600,       // how long (in ms) the animation takes
     activeClass: 'active', // class given to the active nav element
     onPageChange: null,    // function(pageIndex) that is called when page is changed
-    topOffset: 0           // offste (in px) for fixed top navigation
+    topOffset: 100           // offste (in px) for fixed top navigation
   });
 
   // scrollup bottom to top
