733536db1d17e56fb948f48c3fa03018ec418ee2
William <williamhdas@gmail.com>
2020-04-09 01:40:10 -0400

    Fixed background glitch

--- js/main.js
+++ js/main.js
@@ -324,6 +324,16 @@ dots:false,
     $("#learn").delay(9000).fadeIn(1000);
   }
 
+  var is_opera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+  var is_Edge = navigator.userAgent.indexOf("Edge") > -1;
+  var is_chrome = !!window.chrome && !is_opera && !is_Edge;
+  var is_explorer= typeof document !== 'undefined' && !!document.documentMode && !is_Edge;
+  var is_firefox = typeof window.InstallTrigger !== 'undefined';
+  var is_safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+  if(!is_chrome) {
+    $(".slider_area").css({"background-attachment": "scroll"});
+  }
   
 
 });
